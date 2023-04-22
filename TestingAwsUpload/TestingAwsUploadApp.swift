d30b85241292d994474492b5e332149d1c96643a
Abinaya Dinesh <abinayadinesh1@gmail.com>
2023-04-22 11:38:33 -0700

    Initial Commit

---
+++ TestingAwsUpload/TestingAwsUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAwsUploadApp.swift
+//  TestingAwsUpload
+//
+//  Created by Abinaya on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingAwsUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
