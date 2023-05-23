5503222e628cc96476b784c1cfebe17bd63221c4
Abinaya Dinesh <abinayadinesh1@gmail.com>
2023-05-23 16:22:32 -0400

    Initial Commit

---
+++ UpdateGridViaView/UpdateGridViaViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateGridViaViewApp.swift
+//  UpdateGridViaView
+//
+//  Created by Abinaya on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct UpdateGridViaViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
